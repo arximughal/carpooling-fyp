981ba90640ea6466fd9dc3fa5f89db88b6d73790
Fasih Arshad <fasihx@hotmail.com>
2021-03-14 04:39:42 -0700

    add homepage and header file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,127 @@
 
 @section('content')
 
-	<button class="btn btn-primary">Hello There...</button>
-	<input type="text" class="form-control" />
+	<div class="container">
+		<br>
+		<br>
+  <h2>Events created by drivers</h2>
+  <br>
+  <br>           
+  <table class="table table-bordered">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th>Event</th>
+    <td>Lahore to Islamabad</td>
+  </tr>
+  <tr>
+  	<th>Time</th>
+    <td>05-05-2021 2pm</td>
+  </tr>
+  <tr>
+  	<th>Vehicle</th>
+    <td>Tank</td>
+  </tr>
+  <tr>
+  	<th>seats</th>
+    <td>4</td>
+  </tr>
+  </table>
+<br>
+  <table class="table table-bordered">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th>Event</th>
+    <td>Lahore to Islamabad</td>
+  </tr>
+  <tr>
+  	<th>Time</th>
+    <td>05-05-2021 2pm</td>
+  </tr>
+  <tr>
+  	<th>Vehicle</th>
+    <td>Tank</td>
+  </tr>
+  <tr>
+  	<th>seats</th>
+    <td>4</td>
+  </tr>
+  </table>
+<br>
+  <table class="table table-bordered">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th>Event</th>
+    <td>Lahore to Islamabad</td>
+  </tr>
+  <tr>
+  	<th>Time</th>
+    <td>05-05-2021 2pm</td>
+  </tr>
+  <tr>
+  	<th>Vehicle</th>
+    <td>Tank</td>
+  </tr>
+  <tr>
+  	<th>seats</th>
+    <td>4</td>
+  </tr>
+  </table>
+<br>
+  <table class="table table-bordered">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th>Event</th>
+    <td>Lahore to Islamabad</td>
+  </tr>
+  <tr>
+  	<th>Time</th>
+    <td>05-05-2021 2pm</td>
+  </tr>
+  <tr>
+  	<th>Vehicle</th>
+    <td>Tank</td>
+  </tr>
+  <tr>
+  	<th>seats</th>
+    <td>4</td>
+  </tr>
+  </table>
+  <br>
+  <table class="table table-bordered">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th>Event</th>
+    <td>Lahore to Islamabad</td>
+  </tr>
+  <tr>
+  	<th>Time</th>
+    <td>05-05-2021 2pm</td>
+  </tr>
+  <tr>
+  	<th>Vehicle</th>
+    <td>Tank</td>
+  </tr>
+  <tr>
+  	<th>seats</th>
+    <td>4</td>
+  </tr>
+  </table>
+</div>
+
 
 @endsection
